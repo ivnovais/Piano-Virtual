bc1d4d659c16b5bbe7e31b3537c067e0a923b957
ivnovais <ivandronovais@gmail.com>
2023-02-23 17:14:55 -0300

    add error sound

---
+++ main.js
@@ -0,0 +1,28 @@
+const keys = document.querySelectorAll('.key');
+function playNote(event) {
+
+  let audioKeyCode = getKeyCode(event);
+  
+  const key = document.querySelector(`[data-key="${audioKeyCode}"]`)
+  const isKeyExists = key;
+  if(!isKeyExists){
+    const errou = new Audio('sounds/faustao-errou.mp3');
+    errou.play();
+  }
+  console.log(key);
+  //Se tecla existe
+}
+function getKeyCode(event) {
+  let keyCode;
+  const isKeyboard = event.type === 'keydown';
+  if (isKeyboard) {
+    keyCode = event.keyCode;
+  } else {
+    keyCode = event.target.dataset.key;
+  }
+  return keyCode;
+}
+keys.forEach((item) => {
+  item.addEventListener('click', playNote);
+});
+window.addEventListener('keydown', playNote);
